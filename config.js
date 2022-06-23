58329489c1071542771c9737bc84dee3990bb14e
xyashluckyx <77104518+xyashluckyx@users.noreply.github.com>
2022-06-23 07:55:30 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAXL0N4VHoaspb_hC27T_RKfis20ThVN_I",
+    authDomain: "storytelling-app-d3fd4.firebaseapp.com",
+    projectId: "storytelling-app-d3fd4",
+    storageBucket: "storytelling-app-d3fd4.appspot.com",
+    messagingSenderId: "680340555986",
+    appId: "1:680340555986:web:f04c296eb9e9136834450b"
+  };
---
+++ screens/Logout.js
@@ -0,0 +1,27 @@
+import React,{Component} from 'react';
+import {Text, View} from 'react-native';
+import firebase from 'firebase';
+
+export default class Logout extends Component{
+    componentDidMount(){
+        firebase.auth().signOut();
+    }
+    render() {
+        if (!this.state.fontsLoaded) {
+          return <AppLoading />;
+        } else {
+          return (
+            <View
+                style={{
+                    flex:1,
+                    justifyContent:"center",
+                    alignItems:"center"
+                }}
+            >
+                <Text>Logout</Text>
+            </View>
+        )
+    }
+}
+}
+
